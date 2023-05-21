eba31a1db49c743454ed1bf0e559e0e0f1a02263
Virendra <virendra.thakur31@gmail.com>
2023-05-21 20:55:20 +0530

    FlyoutPageItem added.

---
+++ Models/FlyoutPageItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiSampleAppFirst.Models
+{
+    public class FlyoutPageItem
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
