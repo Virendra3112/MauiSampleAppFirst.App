4c6543311f54bee208bc203f7ad73e96b1de215f
Virendra <virendra.thakur31@gmail.com>
2023-06-12 22:37:26 +0530

    DashboardPage updated.

--- Views/DashboardPage.xaml.cs
+++ Views/DashboardPage.xaml.cs
@@ -1,9 +1,19 @@
+using MauiSampleAppFirst.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiSampleAppFirst.Views;
 
 public partial class DashboardPage : ContentPage
 {
-	public DashboardPage()
+    ObservableCollection<FlyoutPageItem> categoryList = new ObservableCollection<FlyoutPageItem>();
+    public ObservableCollection<FlyoutPageItem> CategoryList { get { return categoryList; } }
+    public DashboardPage()
 	{
 		InitializeComponent();
+
+        CategoryList.Add(new FlyoutPageItem { Title = "Contacts", MenuIcon = "contacts.png" });
+        CategoryList.Add(new FlyoutPageItem { Title = "Settings", MenuIcon = "settings.png" });
+
+       BindableLayout.SetItemsSource(flexLayout, CategoryList);
 	}
 }
