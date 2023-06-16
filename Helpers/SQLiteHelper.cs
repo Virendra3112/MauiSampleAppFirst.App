2255efe6782e8797923fa13ff6f798500bf99725
Virendra <virendra.thakur31@gmail.com>
2023-06-16 20:31:41 +0530

    SQLiteHelper added.

---
+++ Helpers/SQLiteHelper.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace MauiSampleAppFirst.Helpers
+{
+    public class SQLiteHelper
+    {
+        SQLiteAsyncConnection Database;
+
+        public SQLiteHelper()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(AppConstants.DatabasePath, AppConstants.Flags);
+            //var result = await Database.CreateTableAsync<TodoItem>();
+        }
+    }
+}
---
+++ Helpers/SQLiteOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSampleAppFirst.Helpers
+{
+    public class SQLiteOperations<T> : ISQLiteOperations<T>
+    {
+        public T GetItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
