30636a81f65940ccb1f4fca9e141694fe5ed52b3
Virendra <virendra.thakur31@gmail.com>
2023-06-19 11:21:59 +0530

    Player model added.

---
+++ Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MauiSampleAppFirst.Models
+{
+    public class Player
+    {
+        [AutoIncrement, PrimaryKey]
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerImage { get; set; }
+        public string Country { get; set; }
+    }
+}
