cf3770bddb36cf719dc4a59ec67248f3ae051a19
Virendra <virendra.thakur31@gmail.com>
2023-06-21 21:06:11 +0530

    DatabaseOperationsSamplePage.xaml.cs updated.

--- Views/DatabaseOperationsSamplePage.xaml.cs
+++ Views/DatabaseOperationsSamplePage.xaml.cs
@@ -1,21 +1,77 @@
 using MauiSampleAppFirst.Helpers;
 using MauiSampleAppFirst.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiSampleAppFirst.Views;
 
 public partial class DatabaseOperationsSamplePage : ContentPage
 {
+    ObservableCollection<Player> players = new ObservableCollection<Player>();
+    public ObservableCollection<Player> Players { get { return players; } }
+
+    //bool isAddPopupVisible = false;
+    //public bool IsAddPopupVisible { get { return isAddPopupVisible; } }
     public DatabaseOperationsSamplePage()
     {
         InitializeComponent();
+        AddDataPopup.IsVisible = false;
     }
 
-    protected override void OnAppearing()
+    protected override async void OnAppearing()
     {
         base.OnAppearing();
 
-        //var addData = DependencyService.Get<ISQLiteOperations<FlyoutPageItem>>();
+       // var addData = DependencyService.Get<ISQLiteOperations<Player>>();
 
-        //addData.InsertAsync(new FlyoutPageItem());
+        //var et = await addData.GetAllAsync();
+
+        //var test =  await SQLiteHelper.Database.GetAsync<Player>(1);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        AddDataPopup.IsVisible = true;
+    }
+
+    private void Submit_Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            AddDataPopup.IsVisible = false;
+
+            //Get data from entry
+
+            var plaerName = playerNameEntry.Text;
+            var plaerCountry = countryName.Text;
+
+            if (!string.IsNullOrEmpty(plaerName) &&
+                !string.IsNullOrEmpty(plaerCountry))
+            {
+
+                var addData = DependencyService.Get<ISQLiteOperations<Player>>();
+
+                addData.InsertAsync(new Player { PlayerName = plaerName, Country = plaerCountry });
+
+            }
+
+            else
+            {
+                //error popup
+            }
+
+
+            //refresh data 
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    private void Close_Button_Clicked(object sender, EventArgs e)
+    {
+        AddDataPopup.IsVisible = false;
+        playerNameEntry.Text = string.Empty;
+        countryName.Text = string.Empty;
     }
 }
