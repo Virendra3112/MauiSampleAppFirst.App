be12e7fac9388e5feede6df0d233224b56ed0da1
Virendra <virendra.thakur31@gmail.com>
2023-06-22 20:43:54 +0530

    DependencyService registered.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MauiSampleAppFirst.Views;
+﻿using MauiSampleAppFirst.Helpers;
+using MauiSampleAppFirst.Models;
+using MauiSampleAppFirst.Views;
 
 namespace MauiSampleAppFirst;
 
@@ -17,6 +19,11 @@ public partial class App : Application
         //MainPage = new DateTimePickerSamplePage();
 
         MainPage = new NavigationPage(new DashboardPage());
+
+        SQLiteHelper.Init();
+
+        DependencyService.Register<ISQLiteOperations<Player>, SQLiteOperations<Player>>();
+
 
         //Routing.RegisterRoute("DashboardTabPage/CollectionViewSamplePage", typeof(CollectionViewSamplePage));
 
