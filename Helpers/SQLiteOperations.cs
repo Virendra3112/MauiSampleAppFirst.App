6c39f3051bc87a8927cf42bf42d4287b138453be
Virendra <virendra.thakur31@gmail.com>
2023-06-23 21:11:22 +0530

    SQLiteOperations updated.

--- Helpers/SQLiteOperations.cs
+++ Helpers/SQLiteOperations.cs
@@ -4,30 +4,30 @@ namespace MauiSampleAppFirst.Helpers
 {
     public class SQLiteOperations<T> : ISQLiteOperations<T> where T : class, new()
     {
-        SQLiteHelper SQLiteHelper;
+        //SQLiteHelper SQLiteHelper;
 
         public SQLiteOperations()
         {
-            SQLiteHelper = new SQLiteHelper();
+            //SQLiteHelper = new SQLiteHelper();
         }
 
 
 
         public async Task<List<T>> GetAllAsync()
         {
-            await SQLiteHelper.Init();
+            //await SQLiteHelper.Init();
             return await SQLiteHelper.Database.Table<T>().ToListAsync();
         }
 
         public async Task<int> InsertAsync(T entity)
         {
-            await SQLiteHelper.Init();
+            //await SQLiteHelper.Init();
             return await SQLiteHelper.Database.InsertAsync(entity);
         }
 
         public async Task<int> DeleteAsync(T entity)
         {
-            await SQLiteHelper.Init();
+            //await SQLiteHelper.Init();
             return await SQLiteHelper.Database.UpdateAsync(entity);
         }
     }
