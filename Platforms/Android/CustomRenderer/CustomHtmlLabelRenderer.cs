79cb82096affd03bb7ee7b85d79547a6ea6a6b11
Virendra <virendra.thakur31@gmail.com>
2023-07-03 20:56:08 +0530

    CustomHtmlLabelRenderer added.

---
+++ Platforms/Android/CustomRenderer/CustomHtmlLabelRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Microsoft.Maui.Controls.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSampleAppFirst.Platforms.Android.CustomRenderer
+{
+    public class CustomHtmlLabelRenderer : LabelRenderer
+    {
+        public CustomHtmlLabelRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
