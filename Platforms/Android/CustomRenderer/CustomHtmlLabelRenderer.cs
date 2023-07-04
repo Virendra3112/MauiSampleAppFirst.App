f39780419167e59c22f9cb5f36026047da3c924f
Virendra <virendra.thakur31@gmail.com>
2023-07-04 21:28:25 +0530

    CustomHtmlLabelRenderer updated.

--- Platforms/Android/CustomRenderer/CustomHtmlLabelRenderer.cs
+++ Platforms/Android/CustomRenderer/CustomHtmlLabelRenderer.cs
@@ -1,23 +1,51 @@
 ﻿using Android.Content;
+using Android.Text;
+using Android.Widget;
+using MauiSampleAppFirst.Controls;
+using MauiSampleAppFirst.Platforms.Android.CustomRenderer;
+using Microsoft.Maui.Controls.Compatibility;
 using Microsoft.Maui.Controls.Compatibility.Platform.Android;
 using Microsoft.Maui.Controls.Platform;
+using Microsoft.Maui.Handlers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//[assembly: ExportRenderer(typeof(CustomHtmlLabel), typeof(CustomHtmlLabelRenderer))]
 namespace MauiSampleAppFirst.Platforms.Android.CustomRenderer
 {
-    public class CustomHtmlLabelRenderer : LabelRenderer
+    public class CustomHtmlLabelRenderer : LabelHandler// LabelRenderer
     {
-        public CustomHtmlLabelRenderer(Context context) : base(context)
+        public CustomHtmlLabelRenderer()//(Context context) : base(context)
         {
         }
 
-        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
-        {
-            base.OnElementChanged(e);
-        }
+        //override one
+        //protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        //{
+        //    base.OnElementChanged(e);
+
+        //    var view = (CustomHtmlLabel)Element;
+        //    if (view == null) return;
+        //    if (!string.IsNullOrEmpty(Control.Text))
+        //    {
+        //        if (Control.Text.Contains("<a"))
+        //        {
+        //            var a = Control.Text.IndexOf("<a");
+        //            var b = Control.Text.IndexOf("</a>");
+        //            var d = Control.Text.Length;
+        //            var c = Control.Text.Length - Control.Text.IndexOf("</a>");
+        //            int length = b - a + 4;
+
+        //            string code = Control.Text.Substring(a, length);
+        //            Control.SetText(Html.FromHtml(view.Text.ToString().Replace(code, string.Empty)), TextView.BufferType.Spannable);
+        //        }
+        //        else
+        //            Control.SetText(Html.FromHtml(view.Text.ToString()), TextView.BufferType.Spannable);
+        //    }
+
+        //}
     }
 }
