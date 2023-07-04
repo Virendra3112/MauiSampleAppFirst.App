76d41fac037a1c65eb2ec30f906f2f57f576f4b1
Virendra <virendra.thakur31@gmail.com>
2023-07-04 21:28:55 +0530

    MauiProgram updated.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,24 +1,31 @@
 ﻿using Microsoft.Extensions.Logging;
-
 namespace MauiSampleAppFirst;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+         .ConfigureMauiHandlers((handlers) =>
+          {
+#if ANDROID
+                handlers.AddHandler(typeof(MauiSampleAppFirst.Controls.CustomHtmlLabel), typeof(MauiSampleAppFirst.Platforms.Android.CustomRenderer.CustomHtmlLabelRenderer));
+#elif IOS
+              //handlers.AddHandler(typeof(PressableView), typeof(XamarinCustomRenderer.iOS.Renderers.PressableViewRenderer));
+#endif
+          });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
