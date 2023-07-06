5518c7ab879572136f0cd028d40165288d8805bd
Virendra <virendra.thakur31@gmail.com>
2023-07-06 21:42:49 +0530

    Page navigation updated.

--- Views/DashboardPage.xaml.cs
+++ Views/DashboardPage.xaml.cs
@@ -16,15 +16,15 @@ public partial class DashboardPage : ContentPage
         //customLabel.Text = test;
 
 
-        CategoryList.Add(new FlyoutPageItem { Title = "SQLite Operations", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "CollectionView", MenuIcon = "contacts.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "Tabs", MenuIcon = "contacts.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "Flyout", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "Shell Flyout", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "CarouselView", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "DateTime picker", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "Contacts list", MenuIcon = "settings.png" });
-        CategoryList.Add(new FlyoutPageItem { Title = "Shapes", MenuIcon = "settings.png" });
+        CategoryList.Add(new FlyoutPageItem { Title = "SQLite Operations", MenuIcon = "settings.png", Page = new DatabaseOperationsSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "CollectionView", MenuIcon = "contacts.png", Page = new CollectionViewSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "Tabs", MenuIcon = "contacts.png", Page = new AppShell() });
+        CategoryList.Add(new FlyoutPageItem { Title = "Flyout", MenuIcon = "settings.png", Page = new FlyoutSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "Shell Flyout", MenuIcon = "settings.png", Page = new ShellFlyoutSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "CarouselView", MenuIcon = "settings.png", Page = new CarouselViewSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "DateTime picker", MenuIcon = "settings.png", Page = new DateTimePickerSamplePage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "Contacts list", MenuIcon = "settings.png", Page = new ContactListPage() });
+        CategoryList.Add(new FlyoutPageItem { Title = "Shapes", MenuIcon = "settings.png", Page = new ShapesSamplePage() });
 
         BindableLayout.SetItemsSource(flexLayout, CategoryList);
     }
@@ -35,45 +35,60 @@ public partial class DashboardPage : ContentPage
 
         if (item != null)
         {
+            if (item.Page != null)
+                NavigateTo(item.Page);
 
-            switch (item.Title)
-            {
-                case "CollectionView":
-                    Navigation.PushAsync(new CollectionViewSamplePage());
-                    break;
+            //switch (item.Title)
+            //{
+            //    case "CollectionView":
+            //        Navigation.PushAsync(new CollectionViewSamplePage());
+            //        break;
 
-                case "Tabs":
-                    Navigation.PushAsync(new AppShell());
-                    break;
+            //    case "Tabs":
+            //        Navigation.PushAsync(new AppShell());
+            //        break;
 
-                case "Flyout":
-                    Navigation.PushAsync(new FlyoutSamplePage());
-                    break;
+            //    case "Flyout":
+            //        Navigation.PushAsync(new FlyoutSamplePage());
+            //        break;
 
-                case "Shell Flyout":
-                    Navigation.PushAsync(new ShellFlyoutSamplePage());
-                    break;
+            //    case "Shell Flyout":
+            //        Navigation.PushAsync(new ShellFlyoutSamplePage());
+            //        break;
 
-                case "CarouselView":
-                    Navigation.PushAsync(new CarouselViewSamplePage());
+            //    case "CarouselView":
+            //        Navigation.PushAsync(new CarouselViewSamplePage());
 
-                    break;
-                case "DateTime picker": 
-                    Navigation.PushAsync(new DateTimePickerSamplePage());
-                    break;
+            //        break;
+            //    case "DateTime picker":
+            //        Navigation.PushAsync(new DateTimePickerSamplePage());
+            //        break;
 
-                case "Contacts list":
-                    Navigation.PushAsync(new ContactListPage());
-                    break;
+            //    case "Contacts list":
+            //        Navigation.PushAsync(new ContactListPage());
+            //        break;
 
-                case "SQLite Operations":
-                    Navigation.PushAsync(new DatabaseOperationsSamplePage());
-                    break; 
-                
-                case "Shapes":
-                    Navigation.PushAsync(new ShapesSamplePage());
-                    break;
-            }
+            //    case "SQLite Operations":
+            //        Navigation.PushAsync(new DatabaseOperationsSamplePage());
+            //        break;
+
+            //    case "Shapes":
+            //        Navigation.PushAsync(new ShapesSamplePage());
+            //        break;
+            //}
+        }
+    }
+
+    private void NavigateTo(Page page)
+    {
+        try
+        {
+            Navigation.PushAsync(page);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
         }
     }
 }
