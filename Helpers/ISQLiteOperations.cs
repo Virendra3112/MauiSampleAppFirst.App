7731185a088e3c95bf9e47c51a72affab7338743
Virendra <virendra.thakur31@gmail.com>
2023-07-09 15:08:10 +0530

    commit.

--- Helpers/ISQLiteOperations.cs
+++ Helpers/ISQLiteOperations.cs
@@ -2,17 +2,55 @@
 {
     public interface ISQLiteOperations<T> where T : class, new()
     {
+        /// <summary>
+        /// Get all Items
+        /// </summary>
+        /// <param name="entity">Entity or item model</param>
+        /// <returns>List of items</returns>       
         Task<List<T>> GetAllAsync();
 
+        /// <summary>
+        /// Insert Item
+        /// </summary>
+        /// <param name="entity">Entity or item model</param>
+        /// <returns>The numbers of rows updated</returns>       
         Task<int> InsertAsync(T entity);
 
+        /// <summary>
+        /// Dalete Item
+        /// </summary>
+        /// <param name="entity">Entity or item model</param>
+        /// <returns>The numbers of rows updated</returns>       
         Task<int> DeleteAsync(T entity);
 
+        /// <summary>
+        /// Update Item
+        /// </summary>
+        /// <param name="entity">Entity or item model</param>
+        /// <returns>The numbers of rows updated</returns>       
         Task<int> UpdateAsync(T entity);
+
+        /// <summary>
+        /// Query to table
+        /// </summary>
+        /// <param name="entity">Entity or item model</param>
+        /// <param name="query">Query</param>
+        /// <param name="param">Parameters</param>
+        /// <returns>Item or list of items</returns>
         Task<List<T>> QueryAsync(T entity, string query, object[] param);
 
+        /// <summary>
+        /// Get item 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns>Item</returns>
         Task<T> GetItemAsync(T entity);
 
+        /// <summary>
+        ///Get item by Id
+        /// </summary>
+        /// <param name="id">Primary key of table</param>
+        /// <returns>Item</returns>
         Task<T> GetItemById(int id);
     }
 }
