55e69bbcb0c17f6169e33341ce2a7fbe0aa25b8b
Virendra <virendra.thakur31@gmail.com>
2023-07-14 21:51:50 +0530

    Unused code removed.

--- Helpers/SQLiteOperations.cs
+++ Helpers/SQLiteOperations.cs
@@ -4,13 +4,6 @@ namespace MauiSampleAppFirst.Helpers
 {
     public class SQLiteOperations<T> : ISQLiteOperations<T> where T : class, new()
     {
-        //SQLiteHelper SQLiteHelper;
-
-        public SQLiteOperations()
-        {
-            //SQLiteHelper = new SQLiteHelper();
-        }
-
         public async Task<List<T>> GetAllAsync()
         {
             return await SQLiteHelper.Database.Table<T>().ToListAsync();
@@ -29,7 +22,6 @@ namespace MauiSampleAppFirst.Helpers
         public async Task<int> UpdateAsync(T entity)
         {
             return await SQLiteHelper.Database.UpdateAsync(entity);
-            //return await SQLiteHelper.Database.InsertOrReplaceAsync(entity);
         }
         public async Task<List<T>> QueryAsync(T entity, string query, object[] param)
         {
